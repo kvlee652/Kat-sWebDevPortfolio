31906331a0ceda2495355c0e7a90ed3dc5b90f4c
Katherine Lee <kalee@student.neumont.edu>
2019-10-23 14:58:26 -0600

    Added switch statements.

---
+++ index.js
@@ -0,0 +1,25 @@
+var x;
+x = 10;
+
+var y;
+y = 5;
+
+z = x + y;
+console.log(z = x + y);
+
+z = x - y;
+console.log(z = x - y);
+
+z = x * y;
+console.log(z = x * y);
+
+z = x / y;
+console.log(z = x / y);
+
+z = x % y;
+console.log(z = x % y);
+
+
+
+
+
